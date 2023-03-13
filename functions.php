317ed8800aeca83bcf8e494859b0c8593ceefa9a
Ronald Mercado <ronaldmh20@gmail.com>
2023-03-13 15:23:01 -0400

    2 commit - functions

--- functions.php
+++ functions.php
@@ -1,62 +1,95 @@
 <?php
 
-require_once "config.php";
-
-
+// Define variables and initialize with empty values
 
 
 // *****************************  Registration Functions **************************************
 
+$username_err = $password_err = $confirm_password_err = $firstname_err = $lastname_err = "";
+
 // username validation
 function usernameValidation($username){
+    global $username_err;
 
-    if(empty(trim($username))){
-        $username_err = "Please enter a username.";
+    if(empty($username)){        
+        $username_err = "Please enter a username !";
     } 
     elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($username))){
         $username_err = "Username can only contain letters, numbers, and underscores.";
+    }else{
+        return trim($username);
     }
-    return $username;
 }
 
 // password validation
 function passwordValidation($password){    
+    
+    global $password_err;
     // Validate password
     if(empty(trim($password))){
         $password_err = "Please enter a password.";     
     } elseif(strlen(trim($password)) < 6){
-        $password_err = "Password must have atleast 6 characters.";
+        $password_err = "Password must have at least 6 characters.";
+    }else{
+        return $password;
     }    
-    return $password;
+    
 }
 
 // Validate confirm password
 function confirmPasswordValidation($password , $confirm_password){
 
+    global $confirm_password_err;    
+    
     if(empty(trim($confirm_password))){
         
         $confirm_password_err = "Please confirm password.";     
     } 
-    else{       
-        $confirm_password = trim($confirm_password);
-        
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Sorry, you entered 2 different passwords.";
-        }        
+    else if (($confirm_password != $password)) {
+      
+        $confirm_password_err = "Sorry, you entered 2 different passwords.";
+               
+    }else{
+        return $confirm_password;
     }
-    return $confirm_password;
+    
+}
+
+// firstname validation
+function firstnameValidation($firstname){
+    global $firstname_err;
+
+    if(empty(trim($firstname))){        
+        $firstname_err = "Please enter a first name.";
+    }else{
+        return $firstname;
+    }
+    
+}
+
+// lastname validation
+function lastnameValidation($lastname){
+    
+    global $lastname_err;
+
+    if(empty(trim($lastname))){        
+        $lastname_err = "Please enter a last name.";
+    }else{
+        return $lastname;
+    }
+    
 }
 
 // validation no errors
 function errorValidation(){
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err))
+    
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($firstname_err) && empty($lastname_err))
         return true;
+    else
+        return false;
+   
 }
 
-
-
 // ************************* Add below other functions ****************************************
-
-
 
 ?>
