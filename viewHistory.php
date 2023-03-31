5a06ca2bf4f1c49f17bb453edd03dadbd0dac56a
Ronald Mercado <ronaldmh20@gmail.com>
2023-03-31 07:54:09 -0400

    draft viehistory

--- viewHistory.php
+++ viewHistory.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +11,12 @@
 </head>
 <body>
     <?php
+
+
+        /// **********************DRAFT************************************* ///
+
+
+
         require_once "DBMainV3.php";
 
 /*
@@ -19,23 +28,23 @@
 
 */
         
-        // Conexión a la base de datos
+        
         $conn = mysqli_connect("localhost", "root","", "kidsGames");
         
-        // Verificar la conexión
+        
         if (!$conn) {
             die("Error al conectar a la base de datos: " . mysqli_connect_error());
         }
         
-        // Consulta SQL para obtener los datos de la vista
+        
         $sql = "SELECT scoreTime, id, fName, lName, result, livesUsed FROM history";
         
-        // Ejecutar la consulta
+        
         $resultado = mysqli_query($conn, $sql);
         
-        // Verificar si se encontraron resultados
+        
         if (mysqli_num_rows($resultado) > 0) {
-            // Mostrar los datos en una tabla HTML
+            
             echo "<table>";
             echo "<tr><th>scoreTime</th><th>id</th><th>fName</th><th>lName</th><th>result</th><th>livesUsed</th></tr>";
             while($fila = mysqli_fetch_assoc($resultado)) {
@@ -45,8 +54,12 @@
         } else {
             echo "No se encontraron resultados.";
         }
+
+
+
+        /// **********************DRAFT************************************* ///
         
-        // Cerrar la conexión
+       
         mysqli_close($conn);
         ?>
         
